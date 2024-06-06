8bec8013d1f41d3384f2ab5585f1b308d27f651f
Panos <44785948+panvog@users.noreply.github.com>
2024-06-06 04:51:16 +0300

    test

--- include/igl/AABB.cpp
+++ include/igl/AABB.cpp
@@ -1,11 +1,11 @@
-// This file is part of libigl, a simple c++ geometry processing library.
+// This file is part of libigl, a simple c++ geometry processing library. 
 //
 // Copyright (C) 2013 Alec Jacobson <alecjacobson@gmail.com>
 //
 // This Source Code Form is subject to the terms of the Mozilla Public License
 // v. 2.0. If a copy of the MPL was not distributed with this file, You can
 // obtain one at http://mozilla.org/MPL/2.0/.
-#include "AABB.h"
+#include "AABB.h" 
 #include "EPS.h"
 #include "barycenter.h"
 #include "colon.h"
